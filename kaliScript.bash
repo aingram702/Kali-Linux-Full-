c763581cba11b5e3820f720338063f81e5b2b734
Andrew <aingram702@outlook.com>
2019-10-30 10:35:33 -0400

    Add files via upload

--- kaliScript.bash
+++ kaliScript.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Step 1: Installing Needed Programs!"
+echo "Step 1: Installing Basic Tools and Programs!"
 cd
 sudo apt-get install -y python3
 sudo apt-get install -y idle
@@ -8,8 +8,8 @@ sudo apt-get install -y steghide
 sudo apt-get install -y exiftool
 sudo apt-get install -y exif
 pip3 install --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint
+cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 2: Installing StegSolve!"
 cd
@@ -19,7 +19,6 @@ mkdir bin
 mv stegsolve.jar bin/ 
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 3: Installing Python Libraries"
 pip install wheel
@@ -58,7 +57,6 @@ pip install py2exe
 pip install keras
 pip install flask
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 4: Installing Stegcracker!"
 cd
@@ -66,22 +64,22 @@ curl https://raw.githubusercontent.com/Paradoxis/StegCracker/master/stegcracker 
 chmod +x /usr/local/bin/stegcracker
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 5: Installing Evil Limiter!"
+cd
 git clone https://github.com/bitbrute/evillimiter.git
 cd evillimiter
 python3 setup.py install
+cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 6: Installing Kali Linux ALL Metapackages!"
 cd
 sudo apt-get install -y kali-linux-all
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 7: Installing TiDoS!"
+cd
 sudo apt-get install -y libncurses5 libxml2 nmap tcpdump libexiv2-dev build-essential python-pip default-libmysqlclient-dev 
 git clone https://github.com/0xinfection/tidos-framework.git
 cd tidos-framework
@@ -89,9 +87,9 @@ chmod +x install
 echo -ne '\n' | ./install 
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 8: Installing Routersploit!"
+cd
 apt-get install python3-pip requests paramiko beautifulsoup pysnmp
 git clone https://github.com/threat9/routersploit
 cd routersploit
@@ -99,7 +97,6 @@ python3 -m pip install -r requirements.txt
 python rsf.py
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 9: Installing LazyScript!"
 cd 
@@ -109,16 +106,17 @@ chmod +x install.sh
 ./install.sh
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 10: Installing Hatch!"
+cd
 git clone https://github.com/MetaChar/Hatch
+cd /Hatch
 python2 main.py
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 11: Installing Fluxion!"
+cd
 apt-get install lighttpd php-cgi pyrit fuser mdk3 awk
 apt-get install isc-dhcp-server
 cd 
@@ -127,27 +125,26 @@ cd fluxion
 sudo ./fluxion.sh
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 12: Installing EvilOSX"
+cd
 git clone https://github.com/Marten4n6/EvilOSX.git
 cd EvilOSX
 pip install -r requirements.txt
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 13: Installing PSPY"
+cd
 git clone https://github.com/DominicBreuker/pspy.git
 cd 
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 14: Installing UserRecon"
+cd
 git clone https://github.com/thelinuxchoice/userrecon.git
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 15: Installing SocialFish"
 cd
@@ -157,24 +154,21 @@ cd SocialFish
 python3 -m pip install -r requirements.txt
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
-cd
 echo "Step 16: Installing Visual Studio Code"
+cd
 sudo apt update
 sudo apt install snapd
 sudo snap install --classic code
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
-cd
 echo "Step 17: Updating and Upgrading Kali-Linux OS!"
+cd
 sudo apt-get update 
 sudo apt-get upgrade -y
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 18: Reconfiguring Packages"
 cd
@@ -184,14 +178,14 @@ sudo dpkg --configure -a
 sudo apt-get update
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
+echo "-------------------------------------------------------------------------------------------------"
 echo "Step 19: Installing ReconT!"
+cd
 git clone https://github.com/jaxBCD/ReconT.git 
 sudo apt install python3 nmap
 pip3 install -r requirements.txt
 cd
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "-------------------------------------------------------------------------------------------------"
 echo "Step 20: Installing FinalRecon!"
 cd
@@ -207,7 +201,6 @@ sudo apt-get install wifiphisher
 cd 
 echo "-------------------------------------------------------------------------------------------------"
 echo "-------------------------------------------------------------------------------------------------"
-echo
 echo "Step 22: Installing WxHexEditor!"
 cd
 sudo apt-get install wxhexeditor
@@ -237,7 +230,6 @@ echo "--------------------------------------------------------------------------
 echo "Step 26: Installing Sonic Visualizer!"
 cd
 wget https://bintray.com/sonic-visualiser/sonic-visualiser/download_file?file_path=sonic-visualiser_4.0_amd64.deb
-cd
 dpkg -i download_file%3Ffile_path=sonic-visualiser_4.0_amd64.deb
 cd
 echo "-------------------------------------------------------------------------------------------------"
